15558edab8b205582d0eae3a811f4e7428b11eee
manueldemeza <manueldemeza26@gmail.com>
2022-01-21 13:01:40 -0600

    Agregamos string al codigo

--- main.cpp
+++ main.cpp
@@ -9,10 +9,12 @@ int main(){
     cout << 10 << endl;
     //imprimiendo un numero flotante (float)
     cout << 3.1416f << endl;
-    //imprimiendo caracteres (char)
+    //imprimiendo string
     cout << "Hola DemezaNews" << endl;
     //imprimiendo un booleano (bool)
     cout << false << endl;
+    //imprimiendo un caracter (char)
+    cout << 'M' << endl;
 
     return 0;
 }
