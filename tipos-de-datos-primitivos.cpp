5f29b6370192cb59caa9763d9d6b070dcf457bc8
manueldemeza <manueldemeza26@gmail.com>
2022-01-21 13:16:37 -0600

    Renombramiento del archivo

---
+++ tipos-de-datos-primitivos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+//funcion principal
+int main(){
+
+    //imprimiendo un numero entero (int)
+    cout << 10 << endl;
+    //imprimiendo un numero flotante (float)
+    cout << 3.1416f << endl;
+    //imprimiendo string
+    cout << "Hola DemezaNews" << endl;
+    //imprimiendo un booleano (bool)
+    cout << false << endl;
+    //imprimiendo un caracter (char)
+    cout << 'M' << endl;
+
+    return 0;
+}
