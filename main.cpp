040e45f755eaf0d8b7210430e55d1a4841a5d342
manueldemeza <manueldemeza26@gmail.com>
2022-01-21 12:42:50 -0600

    Impresion de tipos de datos primitivos

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+//funcion principal
+int main(){
+
+    //imprimiendo un numero entero (int)
+    cout << 10 << endl;
+    //imprimiendo un numero flotante (float)
+    cout << 3.1416f << endl;
+    //imprimiendo caracteres (char)
+    cout << "Hola DemezaNews" << endl;
+    //imprimiendo un booleano (bool)
+    cout << false << endl;
+
+    return 0;
+}
